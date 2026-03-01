df067efc9d6ad9c16350a0da864aa7c10dd400e6
UP230254 <up230254@alumnos.upa.edu.mx>
2026-03-01 12:25:43 -0600

    Tarea

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,65 @@
 module Helper exposing (..)
+
+-- 1.0
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    let
+        evaluateGrade grade =
+            if grade < 0 then
+                Pending
+
+            else if grade > 7 then
+                Approved
+
+            else
+                Failed
+    in
+    List.map evaluateGrade grades
+
+
+
+-- 2.1
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+
+-- 2.2
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+
+-- 2.3
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction statuses =
+    List.map airplaneScheduleAction statuses
